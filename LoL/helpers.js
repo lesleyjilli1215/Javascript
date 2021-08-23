ba1e25a98edc87d5780b3cbc363c1657080b036d
Lesley Pan <lesley.jilli1215@gmail.com>
2021-08-23 12:56:15 +0800

    push

--- LoL/helpers.js
+++
@@ -1,9 +0,0 @@
-function $g(selector) {
-    if(selector.includes('#') && !selector.includes(' ')){
-        return document.querySelector(selector);
-    }
-    
-    let nodelist = document.querySelectorAll(selector);
-
-    return nodelist.length == 1 ? nodelist[0] : nodelist;
-}
